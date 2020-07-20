2c30e372c4de61ba4e1a12e78da86874d6fb4dfc
sx <sx@qq.com>
2020-07-20 16:03:10 +0800

    my first commit

---
+++ src/main/java/com/sx/git/Demo.java
@@ -0,0 +1,12 @@
+package com.sx.git;
+
+import com.sx.pojo.Student;
+
+public class Demo {
+	public static void main(String[] args) {
+		Student stu = new Student();
+		stu.setAge(10);
+		stu.setGender("0");
+		stu.setName("hy");
+	}
+}
